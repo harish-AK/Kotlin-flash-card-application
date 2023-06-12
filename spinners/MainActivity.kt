bf2e02674ac895d7b1deb5753e87bd9efd9d4ad5
HARISH A K <116714372+harish-AK@users.noreply.github.com>
2023-06-12 10:54:43 +0530

    Add files via upload

--- spinners/MainActivity.kt
+++ spinners/MainActivity.kt
@@ -6,16 +6,15 @@ import android.os.Bundle
 import android.view.View
 import android.widget.*
 
-class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     // Declare the category array as a class variable
-    private val category = arrayOf(5000,10000, 15000, 20000)
+    private val category = arrayOf("AIML","CYS","MED","DATA ANALYST")
 
     private var selectedOption: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         // Find the spinner view by its id
         val sp = findViewById<Spinner>(R.id.coursesspinner)
@@ -24,14 +23,14 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         sp.onItemSelectedListener = this
 
         // Create an ArrayAdapter with the category array
-        val adapter: ArrayAdapter<Int> = ArrayAdapter(
+        val adapter: ArrayAdapter<String> = ArrayAdapter(
             this,
             android.R.layout.simple_spinner_item,
             category
         )
 
         // Set the dropdown layout style
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+      //  adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
         // Set the adapter on the spinner
         sp.adapter = adapter
@@ -50,23 +49,40 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             val edit_txt = findViewById<EditText>(R.id.ED1)
             val display = StringBuilder()
             selectedOption = category[position].toString()
-            val selectedOptionInt = selectedOption!!.toInt()
-            val editInt = edit_txt.getText().toString().toInt()
+            val selectedOptionInt = selectedOption
+            val editInt = edit_txt.getText().toString()
+            val tex = findViewById<TextView>(R.id.TV2)
 
-            if (selectedOptionInt != null && editInt != null) {
-                val difference = selectedOptionInt - editInt
-                display.append("You have selected category $selectedOption. You need to buy  for the amount of $difference. to get the prize")
-            } else {
-                display.append("Please enter a valid numeric value.")
+            if (selectedOption =="AIML" && editInt != null) {
+              //  val difference = selectedOptionInt - editInt
+                display.append("You have selected  $selectedOption"+" fee is 200000 $editInt")
+                tex.text = display
+            }
+                if (selectedOption =="CYS" && editInt != null) {
+                    //  val difference = selectedOptionInt - editInt
+                    display.append("You have selected  $selectedOption"+" fee is 300000 $editInt")
+                    tex.text = display
+                }
+                    if (selectedOption =="MED" && editInt != null) {
+                        //  val difference = selectedOptionInt - editInt
+                        display.append("You have selected  $selectedOption"+" fee is 400000 $editInt")
+                        tex.text = display
+                    }
+                        if (selectedOption =="DATA ANALYST" && editInt != null) {
+                            //  val difference = selectedOptionInt - editInt
+                            display.append("You have selected  $selectedOption"+" fee is 200000 $editInt")
+                            tex.text = display
+                        }
             }
 
-            val tex = findViewById<TextView>(R.id.TV2)
-            tex.text = display
+
+
         }
-    }
-    override fun onNothingSelected(parent: AdapterView<*>?) {
-        // Handle the case when no item is selected
-    }
+     override fun onNothingSelected(parent: AdapterView<*>?) {
+         // Handle the case when no item is selected
+     }
+
+ }
 
 
-}
+
